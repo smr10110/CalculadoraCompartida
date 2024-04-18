224ec9e3d2f5d69b7f3e17ee1349dd9d2fa29743
smr10110 <a.huaiquil02@ufromail.cl>
2024-04-17 21:04:35 -0400

    Tercer Commit. perímetros, áreas y volúmenes de figuras geometricas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -39,7 +39,7 @@ public class Main {
         return (numero * porciento) / 100.0;
     }
 
-    // Metodo para calcular el discriminante de la ecuacion cuadratica. Soluciones reales, hacer pruebas unitarias
+    // Metodo para calcular el discriminante de la ecuacion cuadratica
     public static double discriminante(double a, double b, double c) {
         return b * b - 4 * a * c;
     }
@@ -55,7 +55,7 @@ public class Main {
     }
 
     // Método para calcular raíces reales y distintas.
-    private static void calcularRaicesRealesDistintas(double a, double b, double discriminante) {
+    private static void calcularRaicesReales(double a, double b, double discriminante) {
         double root1 = (-b + Math.sqrt(discriminante)) / (2 * a);
         double root2 = (-b - Math.sqrt(discriminante)) / (2 * a);
         System.out.println("Raiz 1 = " + root1);
@@ -76,18 +76,78 @@ public class Main {
         System.out.println("Raíz 2 = " + realPart + " - " + imaginaryPart + "i");
     }
 
-    // Método para calcular las raíces de una ecuación cuadrática, utilizando los métodos auxiliares.
+    // Metodo para calcular las raices de una ecuacion cuadratica, utilizando los metodos auxiliares.
     public static void calcularRaicesCuadraticas(double a, double b, double c) {
         double discriminante = discriminante(a, b, c);
         System.out.println(evaluarDiscriminante(discriminante));
 
         if (discriminante > 0) {
-            calcularRaicesRealesDistintas(a, b, discriminante);
+            calcularRaicesReales(a, b, discriminante);
         } else if (discriminante == 0) {
             calcularRaizRealUnica(a, b);
         } else {
             calcularRaicesComplejas(a, b, discriminante);
         }
+    }
+
+    // Metodo para calcular el perimetro de un cuadrado
+    public static double perimetroCuadrado(double lado) {
+        return 4 * lado;
+    }
+
+    // Metodo para calcular el area de un cuadrado
+    public static double areaCuadrado(double lado) {
+        return lado * lado;
+    }
+
+    // Metodo para calcular el perimetro de un rectangulo
+    public static double perimetroRectangulo(double largo, double ancho) {
+        return 2 * (largo + ancho);
+    }
+
+    // Metodo para calcular el area de un rectangulo
+    public static double areaRectangulo(double largo, double ancho){
+        return largo * ancho;
+    }
+
+    // Metodo para calcular la circunferencia de un circulo
+    public static double circunferenciaCirculo(double radio) {
+        return 2 * Math.PI * radio;
+    }
+
+    // Metodo para calcular el area de un circulo
+    public static double areaCirculo(double radio) {
+        return Math.PI * radio * radio;
+    }
+
+    // Metodo para calcular el area superficial de una esfera
+    public static double areaEsfera(double radio) {
+        return 4 * Math.PI * radio * radio;
+    }
+
+    // Metodo para calcular el volumen de una esfera
+    public static double volumenEsfera(double radio) {
+        return (4.0/3) * Math.PI * Math.pow(radio, 3);
+    }
+
+    // Metodo para calcular el area superficial de un cubo
+    public static double areaCubo(double arista) {
+        return 6 * arista * arista;
+    }
+
+    // Metodo para calcular el volumen de un cubo
+    public static double volumenCubo(double arista) {
+        return Math.pow(arista, 3);
+    }
+
+    // Metodo para calcular el area superficial de un cono
+    public static double areaCono(double radio, double generatriz) {
+        return Math.PI * radio * (radio + generatriz);
+    }
+
+    // Metodo para calcular el volumen de un cono
+    public static double volumenCono(double radio, double altura) {
+        return (Math.PI * Math.pow(radio, 2) * altura) / 3;
     }
 
     public static void main(String[] args) {
