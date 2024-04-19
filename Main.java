93175cc9e9899b153c472c506e215112d5df23ba
TenorioNicolas <n.tenorio01@ufromail.cl>
2024-04-19 18:15:35 -0400

    Sistema de ecuaciones

--- Main.java
+++ Main.java
@@ -1,4 +1,20 @@
 public class Main {
+    public static void resolverSistemaEcuaciones(double A, double B, double C, double D, double E, double F) {
+        double determinante = A * E - B * D;
+
+        if (determinante == 0) {
+            System.out.println("El sistema de ecuaciones no tiene solución única.");
+        } else {
+            double x = (C * E - B * F) / determinante;
+            double y = (A * F - C * D) / determinante;
+
+            // Redondear los resultados a 2 decimales
+            x = Math.round(x * 100.0) / 100.0;
+            y = Math.round(y * 100.0) / 100.0;
+
+            System.out.println("La solución del sistema de ecuaciones es: (x = " + x + ", y = " + y + ")");
+        }
+    }
     public static double calcularPendiente(double x1, double y1, double x2, double y2) {
         return (y2 - y1) / (x2 - x1);
     }
@@ -24,5 +40,15 @@ public class Main {
 
 
         calcularEcuacionRecta(x1, y1, x2, y2);
+
+        double A = 2.0;
+        double B = 3.0;
+        double C = 5.0;
+        double D = 1.0;
+        double E = -1.0;
+        double F = 3.0;
+
+        resolverSistemaEcuaciones(A, B, C, D, E, F);
+
     }
 }
